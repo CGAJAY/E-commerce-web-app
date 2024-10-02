e936d77cdd6e97e92dafead3d6dd4a34fdb3d5e3
CGAJAY <jmugendi737@gmail.com>
2024-10-02 08:31:15 +0300

    Added the userSchema

---
+++ server/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+// User Schema
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	address: {
+		street: String,
+		city: String,
+		zip: String,
+	},
+});
+
+const User = model("User", userSchema);
+export default User;
