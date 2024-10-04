1fb76d2cf8503cf156bd0bf17a69e2421c4ceb2b
CGAJAY <jmugendi737@gmail.com>
2024-10-04 18:14:24 +0300

    linked the database to my server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import userRoutes from "./routes/user.Routes.js"; // Import user routes
+// Import the database connection function
+import { connectDB } from "./db/connectDB.js";
 
 configDotenv(); // Load environment variables
 
-const app = express();
+const app = express(); // Create an Express application
+connectDB(); // Connect to the database
+
+app.use(express.json()); // Middleware to parse JSON request bodies
 
 const PORT = process.env.PORT | 3000;
 
-app.get("/", (req, res) => {
-	res.status(200).send("Hello world");
-});
+// Use user routes for all user-related endpoints
+app.use("/", userRoutes);
 
 app.listen(PORT, () => {
 	console.log("hello");
