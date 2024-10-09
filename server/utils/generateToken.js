49d82dd9e52ccac7e577d3220336ce85d0b78982
CGAJAY <jmugendi737@gmail.com>
2024-10-09 08:39:19 +0300

    Included role of user in my token payload

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,10 +4,13 @@ import { configDotenv } from "dotenv";
 configDotenv(); // Load environment variables
 
 // Function to generate a JWT token
-const generateToken = (userId) => {
+const generateToken = (userId, userRole) => {
 	// Sign the token with the user's ID and return it
 	return jwt.sign(
-		{ id: userId }, // Payload: user's unique ID
+		{
+			id: userId, //Payload: user's unique ID
+			role: userRole, // for role-based access control }, //
+		},
 		process.env.JWT_SECRET, // Secret key (stored in your environment variable)
 		{
 			expiresIn: "30d", // Token expiration (e.g., 30 days)
