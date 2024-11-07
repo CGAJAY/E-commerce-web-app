b8a51d668211ac0e4d13404e55e117b69724edf3
CGAJAY <jmugendi737@gmail.com>
2024-11-07 11:32:10 +0300

    Added react-router-dom, added items.js to hold all products, rendered products on the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "@fontsource/roboto";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
+import ItemsCard from "./components/ ItemsCard";
 
 const App = () => {
 	return (
 		<>
 			<Header />
 			<Carousel />
+			<ItemsCard />
 		</>
 	);
 };
---
+++ client/src/items.js
@@ -0,0 +1,37 @@
+export const items = [
+	{
+		id: 1,
+		imageUrl:
+			"https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "Iphone 16",
+		description:
+			"Latest Iphone 16 with 1tb storage and 64gb ram.",
+		price: 79.99,
+	},
+	{
+		id: 2,
+		imageUrl:
+			"https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "MacBook Pro",
+		description:
+			"MacBook pro Apple M1 chip and 32 Gb memory ",
+		price: 79.99,
+	},
+	{
+		id: 3,
+		imageUrl:
+			"https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "Wireless EHeadphones",
+		description:
+			"High-quality sound with long battery life",
+		price: 79.99,
+	},
+	{
+		id: 4,
+		imageUrl:
+			"https://images.pexels.com/photos/4498479/pexels-photo-4498479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "Apple 2 watch",
+		description: "Comfortable with long life battery",
+		price: 79.99,
+	},
+];
