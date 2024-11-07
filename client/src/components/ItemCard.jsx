b73e40876048f9ab3743876549101cecf2986de1
CGAJAY <jmugendi737@gmail.com>
2024-11-07 11:28:49 +0300

    Added a component that render item details

---
+++ client/src/components/ItemCard.jsx
@@ -0,0 +1,39 @@
+import { FaShoppingBag, FaPlus } from "react-icons/fa";
+
+const ItemCard = ({ item }) => {
+	return (
+		<div className="flex flex-col rounded-lg shadow-md overflow-hidden cursor-pointer">
+			<div className="relative h-48 md:h-64 lg:h-80">
+				<img
+					src={item.imageUrl}
+					alt={item.name}
+					className="w-full h-full object-cover"
+				/>
+				{/* Add to Cart Button with FaShoppingBag and FaPlus icons */}
+				<button
+					className="absolute top-2 left-2 p-2 bg-white rounded-full shadow-md hover:bg-gray-100 transition-colors duration-200"
+					aria-label="Add to cart"
+				>
+					<div className="relative">
+						<FaShoppingBag className="w-6 h-6 text-blue-500" />
+						{/* Plus Icon in Top Right of Shopping Bag */}
+						<FaPlus className="absolute -top-1 -right-2 w-3 h-3  text-blue-500 rounded-full" />
+					</div>
+				</button>
+			</div>
+			<div className="p-4 bg-white">
+				<h3 className="text-lg font-semibold text-gray-800 mb-2">
+					{item.name}
+				</h3>
+				<p className="text-gray-600 mb-2">
+					{item.description}
+				</p>
+				<p className="text-xl font-bold text-black">
+					${item.price.toFixed(2)}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default ItemCard;
