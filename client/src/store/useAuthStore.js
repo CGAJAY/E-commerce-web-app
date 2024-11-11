864b3cdb77b6aa923612938d63447f800db01e94
CGAJAY <jmugendi737@gmail.com>
2024-11-11 21:52:02 +0300

    update user store to allow logout

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,35 +1,12 @@
-// import { create } from "zustand";
-
-// const useAuthStore = create((set) => ({
-// 	// null when logged out
-// 	user:
-// 		// {
-// 		// firstName: "John",
-// 		// lastName: "Doe",
-// 		// username: "jdoe",
-// 		// email: "john.doe@example.com",
-// 		// },
-// 		null,
-// 	login: (userData) => set({ user: userData }),
-// 	logout: () => set({ user: null }),
-// }));
-
-// export default useAuthStore;
-
 import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
+	// set to null, meaning no one is logged in by default.
 	user: null,
-	login: (userData) => set({ user: userData }), // Store user info
+	// function that accepts userData
+	login: (userData) => set({ user: userData }),
+	// function that clears the user state by setting it to null
 	logout: () => set({ user: null }), // Logout user (clear state)
-	initialize: () => {
-		// Check if the user is already logged in on page load (using cookie)
-		const storedUser = localStorage.getItem("user");
-
-		if (storedUser) {
-			set({ user: JSON.parse(storedUser) });
-		}
-	},
 }));
 
 export default useAuthStore;
