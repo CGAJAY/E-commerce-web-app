a04d8c2fcba3af286aa94f940760a02ea5a44039
CGAJAY <jmugendi737@gmail.com>
2024-11-16 13:22:12 +0300

    Rendered user profile photo

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../store/useAuthStore";
+import { FaCamera } from "react-icons/fa"; // FontAwesome camera icon
 
 const Profile = () => {
-	const { user, logout } = useAuthStore();
+	const { user, logout, updateProfilePhoto } =
+		useAuthStore();
 	const navigate = useNavigate();
+
 	const handleLogout = async (e) => {
 		e.preventDefault();
 
@@ -71,18 +74,40 @@ const Profile = () => {
 			<h2 className="text-3xl font-semibold mb-4">
 				My Profile
 			</h2>
+			<div className="flex flex-col items-center mb-6">
+				{/* Profile photo display */}
+				{user.profilePhoto && (
+					<div className="relative">
+						<div className="w-48 h-48 rounded-full bg-gray-300 border  mb-4 flex items-center justify-center">
+							<img
+								src={user.profilePhoto}
+								alt="Profile"
+								className="w-full h-full object-cover rounded-full"
+							/>
+							{/* Font Awesome Camera Icon */}
+							<span className="bg-blue-500 rounded-full  absolute z-50 bottom-5 right-1 p-3">
+								<FaCamera size={30} color="white" />
+							</span>
+						</div>
+					</div>
+				)}
+				{/* if no profile photo */}
+				{!user.profilePhoto && (
+					<div className="relative">
+						<div className="w-48 h-48 rounded-full bg-gray-300 border  mb-4 flex items-center justify-center">
+							{/* Font Awesome Camera Icon */}
+							<span className="bg-green-500 rounded-full  absolute z-50 bottom-5 right-1 p-3">
+								<FaCamera size={30} color="white" />
+							</span>
+						</div>
+					</div>
+				)}
+			</div>
 			<p className="text-lg mb-4">
 				Welcome, {user.username}!
 			</p>
 			<p>Email: {user.email}</p>
-			{user.address && (
-				<>
-					<p>Address:</p>
-					<p>Street: {user.address.street}</p>
-					<p>City: {user.address.city}</p>
-					<p>ZIP: {user.address.zip}</p>
-				</>
-			)}
+
 			<button
 				className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
 				onClick={handleLogout}
