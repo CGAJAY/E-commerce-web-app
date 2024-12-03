e334e6f6b709ebe51dd1c2becbe723737f6f6196
CGAJAY <jmugendi737@gmail.com>
2024-12-03 09:49:08 +0300

    Homepage conditional rendering based on user role

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Carousel from "../components/Carousel";
 import ItemsCard from "../components/ItemsCard";
+import useAuthStore from "../store/useAuthStore";
 
 const HomePage = () => {
+	// Access user state
+	const { user, isAuthenticated } = useAuthStore(
+		(state) => state
+	);
+
+	// Render a different UI if the user is an admin
+	if (isAuthenticated && user?.role === "admin") {
+		return (
+			<div>
+				<h1>Welcome Admin!</h1>
+			</div>
+		);
+	}
 	return (
 		<>
 			<Carousel />
